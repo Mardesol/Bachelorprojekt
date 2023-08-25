ba078b260866521b750bfbf3a21e79fc32b2d5fd
Markus <mfau@itu.dk>
2023-08-25 20:15:39 +0200

    Add functional Cuda setup with addition-1000 implementation

--- CUDA/1000.cu
+++ CUDA/1000.cu
@@ -1,9 +1,12 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 // CUDA kernel to add two matrices in parallel
-__global__ void matrixAddition(int *M1, int *M2, int *M3, int N) {
+__global__ void matrixAddition(int* M1, int* M2, int* M3, int N) {
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
 
@@ -14,45 +17,35 @@ __global__ void matrixAddition(int *M1, int *M2, int *M3, int N) {
 }
 
 int main() {
+    // Start measuring time OS spends on process
+    clock_t setupBegin = clock();
+
     int N = 1000;                // Length of rows and cols
-    char file[] = "1000.txt";    // Name of file
-    
-    // Open first matrix file
-    FILE *file1 = fopen(file, "r");
-    if (file1 == NULL) {
-        perror("Unable to open file");
-        return 1;
-    }
-    
-    // Open second matrix file
-    FILE *file2 = fopen(file, "r");
-    if (file2 == NULL) {
-        perror("Unable to open file");
-        return 1;
-    }
+    int pattern[] = { 1, 2, 3 };
 
     // Allocate memory for matrices on the CPU
-    int *host_M1 = (int *)malloc(N * N * sizeof(int));
-    int *host_M2 = (int *)malloc(N * N * sizeof(int));
-    int *host_M3 = (int *)malloc(N * N * sizeof(int));
+    int* host_M1 = (int*)malloc(N * N * sizeof(int));
+    int* host_M2 = (int*)malloc(N * N * sizeof(int));
+    int* host_M3 = (int*)malloc(N * N * sizeof(int));
 
-    // Read data into host_M1 and host_M2
+    // Initialize matrices M1 and M2 with the pattern
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            fscanf(file1, "%d", &host_M1[i * N + j]);
-            fscanf(file2, "%d", &host_M2[i * N + j]);
+            host_M1[i * N + j] = pattern[j % 3];
+            host_M2[i * N + j] = pattern[j % 3];
         }
     }
 
-    // Close files
-    fclose(file1);
-    fclose(file2);
+    // End measuring time OS spends on process
+    clock_t setupEnd = clock();
+    double time_spent1 = (double)(setupEnd - setupBegin) / CLOCKS_PER_SEC;
+    printf("Time spent on setup: %f seconds\n", time_spent1);
 
     // Allocate memory for matrices on the GPU
-    int *device_M1, *device_M2, *device_M3;
-    cudaMalloc((void **)&device_M1, N * N * sizeof(int));
-    cudaMalloc((void **)&device_M2, N * N * sizeof(int));
-    cudaMalloc((void **)&device_M3, N * N * sizeof(int));
+    int* device_M1, * device_M2, * device_M3;
+    cudaMalloc((void**)&device_M1, N * N * sizeof(int));
+    cudaMalloc((void**)&device_M2, N * N * sizeof(int));
+    cudaMalloc((void**)&device_M3, N * N * sizeof(int));
 
     // Copy input matrices from host to device
     cudaMemcpy(device_M1, host_M1, N * N * sizeof(int), cudaMemcpyHostToDevice);
@@ -66,7 +59,7 @@ int main() {
     clock_t begin = clock();
 
     // Launch the CUDA kernel to perform matrix addition
-    matrixAddition<<<gridDim, blockDim>>>(device_M1, device_M2, device_M3, N);
+    matrixAddition <<<gridDim, blockDim>>> (device_M1, device_M2, device_M3, N);
 
     // End measuring time for matrix addition on the GPU
     clock_t end = clock();
@@ -77,7 +70,7 @@ int main() {
     cudaMemcpy(host_M3, device_M3, N * N * sizeof(int), cudaMemcpyDeviceToHost);
 
     // Open a new file to write the result into
-    FILE *outputFile = fopen("result.txt", "w");
+    FILE* outputFile = fopen("result.txt", "w");
     if (outputFile == NULL) {
         perror("Unable to create the output file");
         return 1;
