f8b8935bfe96caad30a633d80c1e42a228d83540
Markus <mfau@itu.dk>
2023-09-03 13:55:56 +0200

    Make addition take two separate matrices and make addition and multiplication implementations the same

--- CUDA/addition.cu
+++ CUDA/addition.cu
@@ -5,23 +5,25 @@
 #include <stdlib.h>
 #include <time.h>
 
+#include "matrix.cuh"
+
 
 // CUDA kernel to add two matrices
-__global__ void matrixAdditionSimple(int* M1, int* M2, int* M3, int N) {
+__global__ void matrixAdditionSimple(int* M1, int* M2, int* M3, int M3R, int M3C) {
     int index = blockIdx.x * blockDim.x + threadIdx.x;
 
-    if (index < N * N) {
+    if (index < M3R * M3C) {
         M3[index] = M1[index] + M2[index];
     }
 }
 
 // CUDA kernel to add two matrices in parallel
-__global__ void matrixAddition(int* M1, int* M2, int* M3, int N) {
+__global__ void matrixAddition(int* M1, int* M2, int* M3, int M3R, int M3C) {
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
 
-    if (row < N && col < N) {
-        int index = row * N + col;
+    if (row < M3R && col < M3C) {
+        int index = row * M3R + col;
         M3[index] = M1[index] + M2[index];
     }
 }
@@ -40,21 +42,25 @@ int main() {
     // Start the setup timer
     cudaEventRecord(start, 0);
 
-    int N = 1000;                // Length of rows and cols
-    int pattern[] = { 1, 2, 3 };
+    // Define variables
+    Matrix M1;
+    Matrix M2;
+    Matrix M3;
+    int M1Rows = 5;
+    int M1Cols = 5;
+    int M2Rows = 5;
+    int M2Cols = 5;
+    int M3Rows = M1Rows;
+    int M3Cols = M1Cols;
 
-    // Allocate memory for matrices on the CPU
-    int* host_M1 = (int*)malloc(N * N * sizeof(int));
-    int* host_M2 = (int*)malloc(N * N * sizeof(int));
-    int* host_M3 = (int*)malloc(N * N * sizeof(int));
+    // Create the matrix objects
+    M1 = createMatrix(M1Rows, M1Cols);
+    M2 = createMatrix(M2Rows, M2Cols);
+    M3 = createMatrix(M3Rows, M3Cols);
 
-    // Initialize matrices M1 and M2 with the pattern
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            host_M1[i * N + j] = pattern[j % 3];
-            host_M2[i * N + j] = pattern[j % 3];
-        }
-    }
+    // Populate the matrices
+    populateWithOnes(M1);
+    populateWithOnes(M2);
 
     // Stop the setup timer
     cudaEventRecord(stop, 0);
@@ -62,19 +68,20 @@ int main() {
     cudaEventElapsedTime(&setupTime, start, stop);
     printf("Time spent on setup:                      %f seconds\n", setupTime);
 
-
     // Start the data transfer timer (CPU -> GPU / Host -> Device)
     cudaEventRecord(start, 0);
 
     // Allocate memory for matrices on the GPU
     int* device_M1, * device_M2, * device_M3;
-    cudaMalloc((void**)&device_M1, N * N * sizeof(int));
-    cudaMalloc((void**)&device_M2, N * N * sizeof(int));
-    cudaMalloc((void**)&device_M3, N * N * sizeof(int));
+
+    // Allocate memory for matrices on the GPU
+    cudaMalloc((void**)&device_M1, M1Rows * M1Cols * sizeof(int));
+    cudaMalloc((void**)&device_M2, M2Rows * M2Cols * sizeof(int));
+    cudaMalloc((void**)&device_M3, M3Rows * M3Cols * sizeof(int));
 
     // Copy input matrices from host to device
-    cudaMemcpy(device_M1, host_M1, N * N * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(device_M2, host_M2, N * N * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(device_M1, M1.data, M1Rows * M1Cols * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(device_M2, M2.data, M2Rows * M2Cols * sizeof(int), cudaMemcpyHostToDevice);
 
     // Stop the data transfer timer (CPU -> GPU / Host -> Device)
     cudaEventRecord(stop, 0);
@@ -84,13 +91,13 @@ int main() {
 
     // Define block and grid dimensions for CUDA kernel
     dim3 blockDim(32, 32);
-    dim3 gridDim((N + blockDim.x - 1) / blockDim.x, (N + blockDim.y - 1) / blockDim.y);
+    dim3 gridDim((M3Cols + blockDim.x - 1) / blockDim.x, (M3Rows + blockDim.y - 1) / blockDim.y);
 
     // Start the matrix addition timer
     cudaEventRecord(start, 0);
 
     // Launch the CUDA kernel to perform matrix addition
-    matrixAdditionSimple <<<gridDim, blockDim>>> (device_M1, device_M2, device_M3, N);
+    matrixAdditionSimple <<<gridDim, blockDim>>> (device_M1, device_M2, device_M3, M3Rows, M3Cols);
 
     // Stop the matrix addition timer
     cudaEventRecord(stop, 0);
@@ -102,7 +109,7 @@ int main() {
     cudaEventRecord(start, 0);
 
     // Copy the result matrix from device to host
-    cudaMemcpy(host_M3, device_M3, N * N * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(M3.data, device_M3, M3Rows * M3Cols * sizeof(int), cudaMemcpyDeviceToHost);
 
     // Stop the  data transfer timer (GPU -> CPU / Device -> Host)
     cudaEventRecord(stop, 0);
@@ -118,9 +125,9 @@ int main() {
     }
 
     // Write host_M3 to the result file
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            fprintf(outputFile, "%d ", host_M3[i * N + j]);
+    for (int i = 0; i < M3Rows; i++) {
+        for (int j = 0; j < M3Cols; j++) {
+            fprintf(outputFile, "%d ", M3.data[i * M3Rows + j]);
         }
         fprintf(outputFile, "\n");
     }
@@ -132,9 +139,6 @@ int main() {
     cudaFree(device_M1);
     cudaFree(device_M2);
     cudaFree(device_M3);
-    free(host_M1);
-    free(host_M2);
-    free(host_M3);
 
     // End program
     return 0;
