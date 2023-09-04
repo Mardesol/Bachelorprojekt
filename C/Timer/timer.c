9f259a3cae5336b7b1300fba9ac89c32b734f0d1
Dagrún Eir <daas@itu.dk>
2023-09-04 10:39:11 +0200

    Move timer implementation to separate file

---
+++ C/Timer/timer.c
@@ -0,0 +1,28 @@
+#include "timer.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <time.h>
+
+struct Timer createTimer(struct timeval begin, struct timeval end)
+{
+	struct Timer timer;
+	timer.begin = begin;
+	timer.end = end;
+
+	return timer;
+}
+
+void beginTimer(struct Timer *timer)
+{
+	gettimeofday(&timer->begin, 0);
+}
+
+void endTimer(struct Timer *timer, char *message, size_t messageLength)
+{
+	gettimeofday(&timer->end, 0);
+	long seconds = timer->end.tv_sec - timer->begin.tv_sec;
+	long microseconds = timer->end.tv_usec - timer->begin.tv_usec;
+	double elapsed = seconds + microseconds * 1e-6;
+	printf("Time spent on %s: %f seconds\n",message, elapsed);
+}
